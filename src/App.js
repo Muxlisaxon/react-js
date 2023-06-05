f27ee327334e233fd49c741ce516ad5678b0ed9c
Muxlisaxon <qochqorovamuxlisam@gmail.com>
2023-06-05 18:56:19 +0500

    react

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+
+
+import React from "react";
+import Button from "./components/Button"
+
+function App() {
+  return (
+   <>
+   <Button name={"Button"} className={"green"}/>
+   <br/>
+   <br/>
+   <Button name={"Button"} className={"yellow"}/>
+   <br/>
+   <br/>
+   <Button name={"Button"} className={"red"}/>
+   <br/>
+   <br/>
+   <Button name={"Button"} className={"orange"}/>
+   <br/>
+   <br/>
+   <Button name={"Button"} className={"blue"}/>
+   <br/>
+   <br/>
+   <Button name={"Button"} className={"brown"}/>
+   <br/>
+   <br/>
+   <Button name={"Button"} className={"black"}/>
+   <br/>
+   <br/>
+   <Button name={"Button"} className={"blueviolet"}/>
+   <br/>
+   <br/>
+   <Button name={"Button"} className={"chartreuse"}/>
+   <br/>
+   <br/>
+   <Button name={"Button"} className={"darkturquoise"}/>
+   <br/>
+   <br/>
+   </>
+  );
+}
+
+export default App;
